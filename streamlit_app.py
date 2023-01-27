ff9541f0d47ddef719d3ee032dd7f1ce9849080e
AnthonyLam-BDP <123728435+AnthonyLam-BDP@users.noreply.github.com>
2023-01-27 09:35:29 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,9 @@ import streamlit
 
 streamlit.title('My Parent\'s new health dinner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blackberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard boiled Free-Range Egg')
-
+streamlit.header('Breakfast Favourites')
+streamlit.text('🥣 Omega 3 & Blackberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard boiled Free-Range Egg')
+streamlit.text(' 🥑🍞  Avocado Toast')
                 
